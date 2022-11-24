deda7258bed0918c1952cf14f99c31547df2ee30
Yasserhassan_95 <Yasserhassan@1>
2022-11-24 14:04:35 +0200

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal(); //singleton or single instance
+
+  factory MyApp() => _instance; //factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_arch/app/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
