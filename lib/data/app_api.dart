374374d98e994637c4bd78a276b86b7a348ea46d
Yasserhassan_95 <Yasserhassan@1>
2022-12-11 21:49:11 +0200

    add api service client retrofit and generate dio code

---
+++ lib/data/app_api.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_arch/app/constants.dart';
+import 'package:flutter_clean_arch/data/responses.dart';
+import 'package:retrofit/http.dart';
+
+part 'app_api.g.dart';
+
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email, @Field("password") String password);
+}
--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_arch/presentation/resources/styles_manager.dart';
 import 'package:flutter_clean_arch/presentation/resources/values_manager.dart';
@@ -27,7 +26,7 @@ ThemeData getApplicationTheme() {
         elevation: AppSize.s4,
         shadowColor: ColorManager.lightPrimary,
         titleTextStyle:
-        getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
+            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
     // button theme
     buttonTheme: ButtonThemeData(
         shape: const StadiumBorder(),
@@ -51,45 +50,45 @@ ThemeData getApplicationTheme() {
             color: ColorManager.darkGrey, fontSize: FontSize.s16),
         headlineMedium: getRegularStyle(
             color: ColorManager.darkGrey, fontSize: FontSize.s14),
-        titleMedium: getMediumStyle(
-            color: ColorManager.primary, fontSize: FontSize.s16),
-        titleSmall: getRegularStyle(
-            color: ColorManager.white, fontSize: FontSize.s16),
+        titleMedium:
+            getMediumStyle(color: ColorManager.primary, fontSize: FontSize.s16),
+        titleSmall:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
         bodyLarge: getRegularStyle(color: ColorManager.grey1),
         bodySmall: getRegularStyle(color: ColorManager.grey)),
 
     // input decoration theme (text form field)
     inputDecorationTheme: InputDecorationTheme(
-      // content padding
+        // content padding
         contentPadding: const EdgeInsets.all(AppPadding.p8),
         // hint style
         hintStyle:
-        getRegularStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+            getRegularStyle(color: ColorManager.grey, fontSize: FontSize.s14),
         labelStyle:
-        getMediumStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+            getMediumStyle(color: ColorManager.grey, fontSize: FontSize.s14),
         errorStyle: getRegularStyle(color: ColorManager.error),
 
         // enabled border style
         enabledBorder: OutlineInputBorder(
             borderSide:
-            BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+                BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
             borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
 
         // focused border style
         focusedBorder: OutlineInputBorder(
             borderSide:
-            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
             borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
 
         // error border style
         errorBorder: OutlineInputBorder(
             borderSide:
-            BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+                BorderSide(color: ColorManager.error, width: AppSize.s1_5),
             borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
         // focused border style
         focusedErrorBorder: OutlineInputBorder(
             borderSide:
-            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
             borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)))),
     // label style
   );
