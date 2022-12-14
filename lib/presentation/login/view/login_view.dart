e58424208730ac2f985a97171bb7411c2d90f483
Yasserhassan_95 <Yasserhassan@1>
2022-12-14 21:28:03 +0200

    finishing login view and viewmodel

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_arch/presentation/login/viewmodel/login_loginViewModel.dart';
 
 import '../../resources/assets_manager.dart';
 import '../../resources/color_manager.dart';
@@ -16,56 +15,34 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  LoginViewModel _loginViewModel = LoginViewModel(_loginUseCase);
-
-  final TextEditingController _userNameTextEditingController =
-      TextEditingController();
-  final TextEditingController _passwordTextEditingController =
-      TextEditingController();
-
+  final LoginViewModel _viewModel = LoginViewModel();
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _userPasswordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  _bing() {
-    _loginViewModel.start();
-    _userNameTextEditingController.addListener(
-        () => _loginViewModel.setUserName(_userNameTextEditingController.text));
-    _passwordTextEditingController.addListener(
-        () => _loginViewModel.setPassword(_passwordTextEditingController.text));
+
+  _bind() {
+    _viewModel.start(); // tell viewmodel, start ur job
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _userPasswordController.addListener(
+            () => _viewModel.setPassword(_userPasswordController.text));
   }
 
   @override
   void initState() {
-    _bing();
+    _bind();
     super.initState();
   }
 
   @override
-  void dispose() {
-    _loginViewModel.dispose();
-    super.dispose();
-  }
-
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorManager.white,
-      body: StreamBuilder(
-        stream: _loginViewModel.outputState,
-        builder: (context, snapshot) {
-          // return snapshot.data?.getScreenWidget(context, _getContentWidget(),
-          //         () {
-          //       _loginViewModel.login();
-          //     }) ??
-          //     _getContentWidget();
-        },
-      ),
-    );
+    return _getContentWidget();
   }
 
   Widget _getContentWidget() {
-    return Container(
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: Container(
         padding: const EdgeInsets.only(top: AppPadding.p100),
         child: SingleChildScrollView(
           child: Form(
@@ -81,11 +58,11 @@ class _LoginViewState extends State<LoginView> {
                   padding: const EdgeInsets.only(
                       left: AppPadding.p28, right: AppPadding.p28),
                   child: StreamBuilder<bool>(
-                      stream: _loginViewModel.outIsUserNameValid,
+                      stream: _viewModel.outIsUserNameValid,
                       builder: (context, snapshot) {
                         return TextFormField(
                           keyboardType: TextInputType.emailAddress,
-                          controller: _userNameTextEditingController,
+                          controller: _userNameController,
                           decoration: InputDecoration(
                               hintText: AppStrings.username,
                               labelText: AppStrings.username,
@@ -102,11 +79,11 @@ class _LoginViewState extends State<LoginView> {
                   padding: const EdgeInsets.only(
                       left: AppPadding.p28, right: AppPadding.p28),
                   child: StreamBuilder<bool>(
-                      stream: _loginViewModel.outIsPasswordValid,
+                      stream: _viewModel.outIsPasswordValid,
                       builder: (context, snapshot) {
                         return TextFormField(
                           keyboardType: TextInputType.visiblePassword,
-                          controller: _passwordTextEditingController,
+                          controller: _userPasswordController,
                           decoration: InputDecoration(
                               hintText: AppStrings.password,
                               labelText: AppStrings.password,
@@ -123,7 +100,7 @@ class _LoginViewState extends State<LoginView> {
                   padding: const EdgeInsets.only(
                       left: AppPadding.p28, right: AppPadding.p28),
                   child: StreamBuilder<bool>(
-                      stream: _loginViewModel.outAreAllInputsValid,
+                      stream: _viewModel.outAreAllInputsValid,
                       builder: (context, snapshot) {
                         return SizedBox(
                           width: double.infinity,
@@ -131,7 +108,7 @@ class _LoginViewState extends State<LoginView> {
                           child: ElevatedButton(
                               onPressed: (snapshot.data ?? false)
                                   ? () {
-                                _loginViewModel.login();
+                                _viewModel.login();
                               }
                                   : null,
                               child: const Text(AppStrings.login)),
@@ -148,7 +125,7 @@ class _LoginViewState extends State<LoginView> {
                       children: [
                         TextButton(
                           onPressed: () {
-                            Navigator.pushNamed(
+                            Navigator.pushReplacementNamed(
                                 context, Routes.forgotPasswordRoute);
                           },
                           child: Text(AppStrings.forgetPassword,
@@ -156,7 +133,7 @@ class _LoginViewState extends State<LoginView> {
                         ),
                         TextButton(
                           onPressed: () {
-                            Navigator.pushNamed(
+                            Navigator.pushReplacementNamed(
                                 context, Routes.registerRoute);
                           },
                           child: Text(AppStrings.registerText,
@@ -167,6 +144,15 @@ class _LoginViewState extends State<LoginView> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
+
--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -6,27 +6,33 @@ import 'package:flutter_clean_arch/presentation/base/baseviewmodel.dart';
 import 'package:flutter_clean_arch/presentation/common/freezed_data_classes.dart';
 
 class LoginViewModel extends BaseViewModel
-    with LoginViewModelInput, LoginViewModelOutputs {
+    with LoginViewModelInputs, LoginViewModelOutputs {
   final StreamController _userNameStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
   final StreamController _passwordStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
 
-  final LoginUseCase _loginUseCase;
-
-  LoginViewModel(this._loginUseCase);
+  final StreamController _areAllInputsValidStreamController =
+  StreamController<void>.broadcast();
 
   var loginObject = LoginObject("", "");
+  // final LoginUseCase _loginUseCase;
 
-  //inputs
+  // LoginViewModel(this._loginUseCase);
+  LoginViewModel();
+
+  // inputs
   @override
   void dispose() {
     _userNameStreamController.close();
     _passwordStreamController.close();
+    _areAllInputsValidStreamController.close();
   }
 
   @override
-  void start() {}
+  void start() {
+    // TODO: implement start
+  }
 
   @override
   Sink get inputPassword => _passwordStreamController.sink;
@@ -35,41 +41,50 @@ class LoginViewModel extends BaseViewModel
   Sink get inputUserName => _userNameStreamController.sink;
 
   @override
-  login() async {
-    (await _loginUseCase.execute(
-            LoginUseCaseInput(loginObject.userName, loginObject.password)))
-        .fold(
-            (failure) => {
-                  //left - > failure
-                },
-            (data) => {
-                  //right - > success
-                });
-  }
+  Sink get inputAreAllInputsValid => _areAllInputsValidStreamController.sink;
 
   @override
   setPassword(String password) {
     inputPassword.add(password);
     loginObject = loginObject.copyWith(password: password);
+    inputAreAllInputsValid.add(null);
   }
 
   @override
   setUserName(String userName) {
     inputUserName.add(userName);
     loginObject = loginObject.copyWith(userName: userName);
+    inputAreAllInputsValid.add(null);
   }
 
-  //outputs
+  @override
+  login() async {
+    // (await _loginUseCase.execute(
+    //         LoginUseCaseInput(loginObject.userName, loginObject.password)))
+    //     .fold(
+    //         (failure) => {
+    //               // left -> failure
+    //               print(failure.message)
+    //             },
+    //         (data) => {
+    //               // right -> data (success)
+    //               print(data.customer?.name)
+    //             });
+  }
+
+  // outputs
+  @override
+  Stream<bool> get outIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
 
   @override
-  // TODO: implement isPasswordValid
-  Stream<bool> get isPasswordValid =>
-      _passwordStreamController.stream.map((password) => password);
+  Stream<bool> get outIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
 
   @override
-  // TODO: implement isUserNameValid
-  Stream<bool> get isUserNameValid =>
-      _userNameStreamController.stream.map((userName) => userName);
+  Stream<bool> get outAreAllInputsValid =>
+      _areAllInputsValidStreamController.stream
+          .map((_) => _areAllInputsValid());
 
   bool _isPasswordValid(String password) {
     return password.isNotEmpty;
@@ -78,15 +93,14 @@ class LoginViewModel extends BaseViewModel
   bool _isUserNameValid(String userName) {
     return userName.isNotEmpty;
   }
-//
-// bool _areAllInputsValid() {
-//   return _isPasswordValid(loginObject.password) &&
-//       _isUserNameValid(loginObject.userName);
-// }
-// }
+
+  bool _areAllInputsValid() {
+    return _isPasswordValid(loginObject.password) &&
+        _isUserNameValid(loginObject.userName);
+  }
 }
 
-abstract class LoginViewModelInput {
+abstract class LoginViewModelInputs {
   setUserName(String userName);
 
   setPassword(String password);
@@ -96,10 +110,14 @@ abstract class LoginViewModelInput {
   Sink get inputUserName;
 
   Sink get inputPassword;
+
+  Sink get inputAreAllInputsValid;
 }
 
 abstract class LoginViewModelOutputs {
-  Stream<bool> get isUserNameValid;
+  Stream<bool> get outIsUserNameValid;
 
-  Stream<bool> get isPasswordValid;
+  Stream<bool> get outIsPasswordValid;
+
+  Stream<bool> get outAreAllInputsValid;
 }
