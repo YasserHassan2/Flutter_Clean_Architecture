66c66429cb448627d669f9d6329b6382046782f9
Yasserhassan_95 <Yasserhassan@1>
2022-12-16 21:46:15 +0200

    adding state renderer class

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -1,11 +1,11 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color primary = const Color(0xffED9728);
   static Color darkGrey = const Color(0xff525252);
   static Color grey = const Color(0xff737477);
   static Color lightGrey = const Color(0xff9E9E9E);
-
+  static Color black = const Color(0xff000000);
 
   // new colors
   static Color darkPrimary = const Color(0xffd17d11);
@@ -14,6 +14,4 @@ class ColorManager {
   static Color grey2 = const Color(0xff797979);
   static Color white = const Color(0xffFFFFFF);
   static Color error = const Color(0xffe61f34); // red color
-
-
 }
